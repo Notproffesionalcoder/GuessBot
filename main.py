083afa6136e9e905ee3a05c23f37eb7283336c6a
py-dev-2 <89005790+py-dev-2@users.noreply.github.com>
2021-08-24 14:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name='.support | By ~Rishi#0100'))
     print(f"Logged in as {client.user.name}")
     print(f"Bot ID: {client.user.id}")
+    
     @client.command()
 async def support(ctx):
     await ctx.send("List of commands:  `.gtn`." )
+    
     @client.command()
 async def gtn(ctx):
     computer = random.randint(1, 10)
